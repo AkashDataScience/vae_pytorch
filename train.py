1145c2d8f5c733214b1cd32b0aaaa0ca86c45893
AkashDataScience <akashmshah19@gmail.com>
2024-07-19 21:20:03 +0530

    Minor fix

--- train.py
+++ train.py
@@ -9,6 +9,8 @@ def get_args():
     parser.add_argument('--epochs', default=30, type=int, help="Number of training epochs e.g 25")
     parser.add_argument('--batch_size', default=32, type=int, help="Number of images per batch e.g. 256")
     parser.add_argument('--mnist', action=argparse.BooleanOptionalAction)
+    args = parser.parse_args()
+    return args
 
 def start_training(num_epochs, model, device, data_loader):
         trainer = pl.Trainer(accelerator = device, max_epochs=num_epochs, progress_bar_refresh_rate=10)
